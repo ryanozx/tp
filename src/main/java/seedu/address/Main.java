503d6df2665203488e37321d4e3f18a502e01f23
Qin Nanxin <nanxinqin@hotmail.com>
2023-10-12 14:52:14 +0800

    Add comment to test dashboard pr detection function

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -20,6 +20,8 @@ import seedu.address.commons.core.LogsCenter;
  *
  * By having a separate main class (Main) that doesn't extend Application
  * to be the entry point of the application, we avoid this issue.
+ * 
+ * This is a line of comment to test the dashboard pr detection function.
  */
 public class Main {
     private static Logger logger = LogsCenter.getLogger(Main.class);
