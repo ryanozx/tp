84be643207c21dca31bc367fd4fd1cc19c1ccf3b
Ivan <103909775+ivyy-poison@users.noreply.github.com>
2023-10-23 22:35:20 +0800

    Implement UniqueLeaveList class

---
+++ src/main/java/seedu/address/model/leave/exceptions/DuplicateLeaveException.java
@@ -0,0 +1,11 @@
+package seedu.address.model.leave.exceptions;
+
+/**
+ * Signals that the operation will result in duplicate Leaves (Leaves are considered duplicates if they have the same
+ * identity).
+ */
+public class DuplicateLeaveException extends RuntimeException {
+    public DuplicateLeaveException() {
+        super("Operation would result in duplicate leaves");
+    }   
+}
