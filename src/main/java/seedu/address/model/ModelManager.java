3de93fe3164600d550c8206e3317feb3e4ff313a
jean-cq <mary84060117@gmail.com>
2023-10-26 14:27:57 +0800

    Test read-only feature of LeaveList and change the tag example

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -126,7 +126,7 @@ public class ModelManager implements Model {
     }
 
     /**
-     * Returns an unmodifiable view of the list of {@code Person} backed by the internal list of
+     * Returns an unmodifiable view of the list of {@code Leave} backed by the internal list of
      * {@code versionedAddressBook}
      */
     @Override
