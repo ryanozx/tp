5a707e6410876239c061e8902aa768a8a8403d1e
jean-cq <mary84060117@gmail.com>
2023-10-27 13:29:12 +0800

    Check the styles

--- src/main/java/seedu/address/logic/commands/FindAllTagCommand.java
+++ src/main/java/seedu/address/logic/commands/FindAllTagCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.logging.Logger;
 
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.util.ToStringBuilder;
@@ -10,7 +11,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.TagsContainAllTagsPredicate;
 
-import java.util.logging.Logger;
+
 
 /**
  * Find Persons with the exact tags
