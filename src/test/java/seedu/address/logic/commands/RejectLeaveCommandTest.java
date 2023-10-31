5bd7a22a615f42e0b6b157227751ed40271c80dd
Qin Nanxin <nanxinqin@hotmail.com>
2023-10-31 20:48:30 +0800

    Update Checkstyle

--- src/test/java/seedu/address/logic/commands/RejectLeaveCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RejectLeaveCommandTest.java
@@ -1,6 +1,15 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalLeaves.getTypicalLeavesBook;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -11,12 +20,6 @@ import seedu.address.model.UserPrefs;
 import seedu.address.model.leave.Leave;
 import seedu.address.testutil.LeaveBuilder;
 import seedu.address.testutil.TestUtil;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalLeaves.getTypicalLeavesBook;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 public class RejectLeaveCommandTest {
 
