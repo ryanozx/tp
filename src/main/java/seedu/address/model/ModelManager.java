238e5268ca2a5f60791b2c2b2e9c9c04ff9c71a8
jean-cq <mary84060117@gmail.com>
2023-11-01 21:03:33 +0800

    resolve trailing whitespace

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -27,6 +27,7 @@ public class ModelManager implements Model {
     private final UserPrefs userPrefs;
     private final FilteredList<Person> filteredPersons;
     private final FilteredList<Leave> filteredLeaves;
+
     /**
      * Initializes a ModelManager with the given addressBook and userPrefs.
      */
