12bb80c7e8bbd4671996669c7eaa50e6b9f07bf6
jean-cq <mary84060117@gmail.com>
2023-11-01 15:17:47 +0800

    Resolve the command instruction

--- src/main/java/seedu/address/logic/commands/FindAllTagCommand.java
+++ src/main/java/seedu/address/logic/commands/FindAllTagCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.logging.Logger;
 
@@ -22,7 +23,8 @@ public class FindAllTagCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all employees whose tags match all "
             + "the specified tags exactly (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: t/TAG [t/MORE_TAGS]...\n"
+            + "Parameters: "
+            + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD
             + " t/full time t/remote\n";
 
--- src/main/java/seedu/address/logic/commands/FindSomeTagCommand.java
+++ src/main/java/seedu/address/logic/commands/FindSomeTagCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
@@ -17,7 +18,8 @@ public class FindSomeTagCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all employees whose tags match some "
             + "specified tags (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: t/TAG [t/MORE_TAGS]...\n"
+            + "Parameters: "
+            + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD
             + " t/full time t/remote\n";
 
